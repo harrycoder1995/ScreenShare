3f2075df73425d8b0674777487537ba12ca1f446
Harendra Rana <harendrarana@Harendras-MacBook-Pro.local>
2024-06-08 12:31:31 +0530

    Initial Commit Project Setup

---
+++ ScreenShare/ScreenShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenShareApp.swift
+//  ScreenShare
+//
+//  Created by Harendra Rana on 08/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ScreenShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
